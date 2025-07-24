f71308ce1f28137f04460b2803a50ae62b7d2364
Sam Van der Plaetsen <samvande12@student.arteveldehs.be>
2025-07-24 16:38:25 +0200

    fix 4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import AdminPage from './pages/AdminPage';
 import GuestPage from './pages/GuestPage';
@@ -17,7 +17,8 @@ function App() {
     if (token && userRole) {
       if (userRole === 'admin') {
         navigate('/admin', { replace: true });
-      } else { // If token exists but role is not admin, redirect to login
+      } else {
+        // If token exists but role is not admin, redirect to login
         navigate('/', { replace: true });
       }
     }
@@ -31,23 +32,17 @@ function App() {
       <Route path="/guest-login/:eventId" element={<GuestLoginPage />} />
 
       {/* Protected Routes */}
-      <Route element={<ProtectedRoute allowedRoles={['admin']} />}>
+      <Route element={<ProtectedRoute allowedRoles={[ 'admin' ]} />}>
         <Route path="/admin" element={<AdminPage />} />
       </Route>
     </Routes>
   );
 }
 
-export default App;
-      <Route path="/guest/:guestId" element={<GuestPage />} />
-      <Route path="/guest-login/:eventId" element={<GuestLoginPage />} />
-
-      {/* Protected Routes */}
-      <Route element={<ProtectedRoute allowedRoles={['admin']} />}>
-        <Route path="/admin" element={<AdminPage />} />
-      </Route>
-    </Routes>
+export default function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   );
 }
-
-export default App;
